ad233385566abcb6b3e2b44750ec4609d5826576
Andrew <andrew.casin@gmail.com>
2020-10-28 23:54:58 -0500

    commit tree 818732bd95cdb96b2eb7dbac198614d299a00197

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+    return false if number < 2 #first prime number is 2
+    for div in 2..(number - 1) #ruby for loop using range
+        if (number % div) == 0 #prime number can't be divisible by anything evenly other than 1 and itself
+            return false
+        end
+    end
+    true
+end
